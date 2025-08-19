cb286165725231709b116382b6c4696c4283d9b7
everaldoinovus <everaldo@inovus.com.br>
2025-08-19 08:09:26 -0300

    Alteração de senha de usuário

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -169,7 +169,7 @@ function DashboardPage() {
                     )}
                     
                     <button 
-						className="btn btn-info user-profile-button" 
+						className="btn btn-info" 
 						onClick={() => setIsProfileModalOpen(true)} 
 						title="Meu Perfil e Configurações"
 					>
